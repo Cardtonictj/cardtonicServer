e27608153e4813c3fa9c682d65e5df8f53b7a926
ezebright898 <ezebright898@gmail.com>
2025-04-19 07:21:31 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "ezebright898@gmail.com";
 const pass = "acujzwuvedbxvuoq";
-// novah 22 march
+// novah 30th march
 
 // Middleware
 app.use(express.json());
